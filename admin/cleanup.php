85793cda244c365f080f3b530b1712758eae82c2
porneL <pornel@pornel.net>
2011-01-08 20:14:30 +0000

    Changed IP/words cleanup tresholds, added AOL proxy to dynip whitelist.

--- admin/cleanup.php
+++ admin/cleanup.php
@@ -17,11 +17,12 @@ class CleanupPage extends AdminPage
 		set f.added=f.added,f.spampoints = f.spampoints/2 where
 		d.host like '%.adsl.tpnet.pl' or
 		d.host like '%.dialog.net.pl' or
+		d.host like '%.proxy.aol.com' or
 		d.host like '%.chello.pl' or
 		d.host like '%.unregistered.net.telenergo.pl' or
 		t.host is not null
 		");
-		$pdo->exec("/*maxtime10*/DELETE from plonker where (spampoints<100 and added < now() - interval 3 month) or spampoints<5");
+		$pdo->exec("/*maxtime10*/DELETE from plonker where (spampoints<50 and added < now() - interval 3 month) or spampoints<5");
 
 		$pdo->exec("/*maxtime10*/DELETE from dnscache where host is NULL or rand()<0.4");
 		$pdo->exec("/*maxtime10*/DELETE from dnsrevcache where ip = 0 or rand()<0.4");
@@ -42,11 +43,11 @@ ip between inet_aton('224.0.0.0') and inet_aton('255.255.255.255');");
 
 		if (date("d")%1)
 		{
-			$pdo->exec("/*maxtime60*/DELETE from bayeswordsh where spam<3 and ham<2 and added < now() - interval 1 month limit 200000");
+			$pdo->exec("/*maxtime60*/DELETE from bayeswordsh where spam<4 and ham<2 and added < now() - interval 2 month limit 200000");
 		}
 		else
 		{
-			$pdo->exec("/*maxtime60*/DELETE from linkswordsh where spam<3 and ham<2 and added < now() - interval 1 month limit 100000");
+			$pdo->exec("/*maxtime60*/DELETE from linkswordsh where spam<3 and ham<2 and added < now() - interval 2 month limit 100000");
 		}
 
 		$pdo->exec("DELETE from plonker WHERE ip BETWEEN  INET_ATON('192.168.0.0') AND INET_ATON('192.168.255.255')");
